c31e165562cbcbc0456976aaea3203f8a87825ad
Kenan <114463518+kenan3256@users.noreply.github.com>
2023-11-26 12:50:47 +0400

    end 26.11.2023

--- src/main/java/org/example/ChinesePostmanRunner.java
+++ src/main/java/org/example/ChinesePostmanRunner.java
@@ -9,15 +9,35 @@ import java.util.*;
 public class ChinesePostmanRunner {
 
     public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int [][] nodes = NodeReader.readNodesFromExcel("C:\\Users\\kenan\\OneDrive\\Desktop\\kenan.xlsx");
-        System.out.print("Başlanğıc nöqteni daxil edin: ");
+        Scanner sc = new Scanner(System.in);
+        int[][] nodes = NodeReader.readNodesFromExcel("C:\\Users\\kenan\\OneDrive\\Desktop\\kenan.xlsx");
+        System.out.print("Başlanğıc nöqtəni daxil edin: ");
 
         int input = sc.nextInt();
-        UndirectedGraph graph = UndirectedGraph.getGraphFromAdjMatrix(nodes);
-    ChinesePostman<UndirectedGraph> chinesePostman = new ChinesePostman<>();
+        boolean flag = true;
+        while (flag) {
 
-        chinesePostman.run(new Dijkstra(graph), graph, input-1);
+            if (input <= 0) {
+                System.out.println("Daxil etdiyiniz nömrə qrafin ölçülərindən kiçikdir");
+                System.out.print("Başlanğıc nöqtəni yenidən daxil edin:");
+                input = sc.nextInt();
+
+
+            } else if (input > nodes.length) {
+                System.out.println("Daxil etdiyiniz nömrə qrafin ölçülərindən böyükdür");
+                System.out.print("Başlanğıc nöqtəni yenidən daxil edin:");
+                input = sc.nextInt();
+
+            } else {
+                flag = false;
+            }
+        }
+
+
+        UndirectedGraph graph = UndirectedGraph.getGraphFromAdjMatrix(nodes);
+        ChinesePostman<UndirectedGraph> chinesePostman = new ChinesePostman<>();
+
+        chinesePostman.run(new Dijkstra(graph), graph, input - 1);
 
 
     }
